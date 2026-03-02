87ed5d4be458a4a80ba5cd847713aeb8a936c3cc
Tsepo Motsatse <tsepom@gmail.com>
2026-03-02 11:15:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9127297601-f30f4",
+  "appId": "1:1050753452160:web:2d69bf0f3f27ef04a97e65",
+  "apiKey": "AIzaSyBhUuQiY5YJ_iLZ2QLR2qT3d2EWS39g6jM",
+  "authDomain": "studio-9127297601-f30f4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1050753452160"
+};
